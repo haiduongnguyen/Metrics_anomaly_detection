099730d808c94999473f0dc49103545301b03e1b
haiduongnguyen <haiduong667799@gmail.com>
2025-11-05 01:40:39 +0700

    update stages ingestion & etl

---
+++ stages/terraform/02-etl/iam.tf
@@ -0,0 +1,37 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "vpbank_lambda_ingest_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "vpbank_lambda_policy"
+  description = "Allow Lambda to access S3 and CloudWatch"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:GetObject", "s3:PutObject", "s3:ListBucket"]
+        Resource = ["arn:aws:s3:::vpbank-hackathon/*"]
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
